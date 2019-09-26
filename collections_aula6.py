73e0677ff8750df12c0b8311f3117bcadc1d58c5
André Arruda <andre23arruda@gmail.com>
2019-09-26 00:23:50 -0300

    feat: Add new files from alura classes

---
+++ collections_aula6.py
@@ -0,0 +1,16 @@
+#%% Ordenando lista com sorted
+idades = [5,3,1,4,7,8,2,3,5]
+idades_new = sorted(idades)
+print(idades_new)
+
+#%% Ordem reversa
+idades_reverse = reversed(idades)
+print(list(idades_reverse))
+
+#%% Ordenado reverso
+idades_new_reverse = sorted(idades,reverse=True)
+idades_new_reverse
+
+#%% Método sort
+idades.sort() # esse muda para sempre
+idades
