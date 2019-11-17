285240d4d1767c4a0ce4e931ad0d195517836352
André Arruda <andre23arruda@gmail.com>
2019-11-17 00:55:58 -0300

    feat: Add function to start the game with another button

--- game-nexus-py/push_button_nexus_game.py.py
+++ game-nexus-py/push_button_nexus_game.py.py
@@ -7,25 +7,24 @@ import pyautogui # comandos no pc
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) # pino 10 -> dino pula
-GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) # pino 11 -> reset
+GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) # pino 11 -> start game
 
-# pular
-def jump_callback(channel): 
+def jump_callback(channel):
     pyautogui.keyDown('space')
     time.sleep(0.025)
     pyautogui.keyUp('space')  
     print("Dino pula")
 
-# encerrar
-def quit_callback(channel):
-    pyautogui.keyDown('enter')
+def start_callback(channel):
+    pyautogui.keyDown('left')
     time.sleep(0.025)
-    pyautogui.keyUp('enter')  
-    print("Encerra o programa")
+    pyautogui.keyUp('left')  
+    print("Começa o jogo")
     return 1
 
 GPIO.add_event_detect(10, GPIO.RISING, callback = jump_callback) # Setup event on pin 10 rising edge
-GPIO.add_event_detect(11, GPIO.RISING, callback = quit_callback) # Setup event on pin 11 rising edge
+GPIO.add_event_detect(11, GPIO.RISING, callback = start_callback) # Setup event on pin 10 rising edge
+
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 GPIO.cleanup() # Clean up
 
